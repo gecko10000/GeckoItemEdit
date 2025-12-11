59c647eb5e7fa8a1805935601610934a90cfe697
gecko10000 <levtey@gmail.com>
2025-12-10 18:00:07 -0800

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("de.eldoria.plugin-yml.bukkit") version "0.6.0"
+    id("de.eldoria.plugin-yml.bukkit") version "0.8.0"
 }
 
 sourceSets {
@@ -34,8 +34,8 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
     compileOnly("gecko10000.geckolib:GeckoLib:1.1")
-    compileOnly("net.strokkur", "commands-annotations", "1.5.0")
-    annotationProcessor("net.strokkur", "commands-processor", "1.5.0")
+    compileOnly("net.strokkur.commands:annotations-paper:2.0.0-SNAPSHOT")
+    annotationProcessor("net.strokkur.commands:processor-paper:2.0.0-SNAPSHOT")
 }
 
 java {
